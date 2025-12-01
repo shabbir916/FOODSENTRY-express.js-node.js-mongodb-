4362358393abf50a5eab539de200fdafec889549
Shabbir Sadriwala <sadriwalashabbir671@gmail.com>
2025-12-01 19:42:30 +0530

    made refinments in expiryDateRange logic

--- src/utils/expiryHelper.js
+++ src/utils/expiryHelper.js
@@ -1,19 +1,25 @@
 const pantryModel = require("../models/pantry.model");
 
-async function getExpiringItems(userId) {
+function expiryDateRange(){
   const today = new Date();
-  const upcomingDate = new Date();
-  upcomingDate.setDate(today.getDate() + 7);
+  today.setHours(0,0,0,0);
+
+  const next7Days = new Date();
+  next7Days.setDate(today.getDate() + 7);
+  next7Days.setHours(23,59,59,999);
+
+  return {today,next7Days};
+}
+
+async function getExpiringItems(userId) {
+  const {today,next7Days} = expiryDateRange();
 
   const expiringItems = await pantryModel.find({
     user: userId,
-    expiryDate: {
-      $gte: new Date(today),
-      $lte: new Date(upcomingDate),
-    },
+    expiryDate: { $gte: today, $lte: next7Days },
   });
 
-  return expiringItems
+  return expiringItems;
 }
 
-module.exports = {getExpiringItems}
+module.exports = { getExpiringItems,expiryDateRange };
