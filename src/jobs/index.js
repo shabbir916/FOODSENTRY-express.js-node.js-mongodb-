baf50d2a2b603532e42df555a805189b88b70896
Shabbir Sadriwala <sadriwalashabbir671@gmail.com>
2025-12-22 14:13:12 +0530

    Refactor(status code):updated status code.

--- src/jobs/index.js
+++ src/jobs/index.js
@@ -1,3 +1,18 @@
+require("dotenv").config();
+const connectDB = require("../db/db");
 const sendExpiryEmails = require("./expiryEmailJob");
 
-sendExpiryEmails();
+(async () => {
+  try {
+    await connectDB();
+    console.log("DB connected for Expiry Email Job");
+
+    await sendExpiryEmails();
+
+    console.log("Expiry Email Job finished successfully");
+    process.exit(0);
+  } catch (error) {
+    console.error("Expiry Email Job failed:", error);
+    process.exit(1);
+  }
+})();
