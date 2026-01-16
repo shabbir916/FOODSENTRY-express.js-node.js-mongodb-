8bb858aeee3518138bd5a891d06f1064b7943755
Shabbir Sadriwala <sadriwalashabbir671@gmail.com>
2026-01-16 11:28:28 +0530

    made some changes in server.js file.

--- server.js
+++ server.js
@@ -1,24 +1,28 @@
 require("dotenv").config();
+
+const http = require("http");
 const app = require("./src/app");
 const connectDB = require("./src/db/db");
 const initSocketServer = require("./src/sockets/socket.server");
-const http = require("http");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000; // Render injects PORT automatically
 
 const httpServer = http.createServer(app);
 
 async function startServer() {
   try {
     await connectDB();
+    console.log("Database connected successfully");
 
     require("./src/cron/expiryCron");
 
     initSocketServer(httpServer);
 
-    httpServer.listen(PORT, () => {
+    httpServer.listen(PORT, "0.0.0.0", () => {
       console.log(
-        `Server started on port ${PORT} in ${process.env.NODE_ENV} mode`
+        `Server running on port ${PORT} in ${
+          process.env.NODE_ENV || "development"
+        } mode`
       );
     });
   } catch (error) {
